e25e85b560f451440ac2735f2c39c2491072a1e1
rcnobles <36048441+rcnobles@users.noreply.github.com>
2021-09-19 18:53:51 -0400

    Update pika_query.sql

--- pika_query.sql
+++ pika_query.sql
@@ -1,1 +1,15 @@
+CREATE TABLE users (
+	user_id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR (20) NOT NULL,
+  last_name VARCHAR (40) NOT NULL,
+  emergency_contact_id INT NOT NULL,
+  allergy_count INT NOT NULL,
+  allergies_text VARCHAR NOT NULL,
+);
 
+CREATE TABLE emergency_contacts (
+	emergency_id INT NOT NULL PRIMARY KEY,
+  emergency_first VARCHAR (20) NOT NULL,
+  emergency_last VARCHAR (40) NOT NULL,
+  emergency_phone VARCHAR (15) INT NOT NULL,
+);
