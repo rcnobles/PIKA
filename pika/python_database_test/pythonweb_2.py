bfd994e6fce30b8c66b90779d9f667007dd2396a
tdamabhorn <tdamabhorn@gmail.com>
2021-10-04 20:06:50 -0700

    initial attempt to connect to database, create new table(s), insert a row with info, and read all entries

---
+++ pika/python_database_test/pythonweb_2.py
@@ -0,0 +1,46 @@
+# This python file connects to the database and inserts a row with information into the database testUserAcc
+# Then this file will also print all entries listed in the table
+
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "",
+    database = 'testUserAcc'
+)
+
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='testUserAcc',
+                                         user='root',
+                                         password='')
+
+
+    #adding data to table
+    mycurser = db.cursor()
+    mycurser.execute("INSERT INTO userAccInfoTest (first_name, last_name, userAccName, userAccPassword, userEmail) VALUES (%s, %s, %s, %s, %s)", ("user1", "firstUser", "testUser1", "1234", "user1@gmail.com"))
+    db.commit()
+
+    sql_select_Query = "select * from userAccInfoTest"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+    print("Total number of rows in table: ", cursor.rowcount)
+
+    print("\nPrinting each row")
+    for row in records:
+        print("User ID = ", row[0], )
+        print("First Name = ", row[1])
+        print("Last Name = ", row[2])
+        print("Username = ", row[3])
+        print("User Email = ", row[5])
+
+except mysql.connector.Error as e:
+    print("Error reading data from MySQL table", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        cursor.close()
+        print("MySQL connection is closed")
