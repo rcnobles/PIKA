22169a677ba115146a7db8714fda351396faf7de
Valerie Martinez <valeriemartnz@csu.fullerton.edu>
2021-10-07 19:48:31 -0700

    changed what database is called, may change back

--- pika/pythonweb.py
+++ pika/pythonweb.py
@@ -5,24 +5,24 @@ import cgi
 
 print("<h1>Welcome to Pika</h1>")
 print("<hr/>")
-print("<h1>Using input tag</h1>")
+print("<h1>record inserted successfully</h1>")
 print("<body bgcolor='blue'>")
 
 form=cgi.FieldStorage()
 
-user_id=form.getvalue("user_id")
-first_name=form.getvalue("first_name")
-last_name=form.getvalue("last_name")
-allergies_text=form.getvalue("allergies_text")
-allergy_count=form.getvalue("allergy_count")
-emergency_contact_id=form.getvalue("emergency_contact_id")
+id=form.getvalue("id")
+firstname=form.getvalue("firstname")
+lastname=form.getvalue("lastname")
+allergy=form.getvalue("allergy")
+allergycount=form.getvalue("allergycount")
+emernum=form.getvalue("emernum")
 
 import mysql.connector
 
-con=mysql.connector.connect(user='root', password='', host='localhost', database='pika')
+con=mysql.connector.connect(user='root', password='', host='localhost', database='allergyweb')
 cur=con.cursor()
 
-cur.execute("insert into user values(%s,%s,%d,%d,%d,%s)",(first_name,last_name,user_id,emergency_contact_id,allergy_count,allergies_text))
+cur.execute("insert into user values(%d,%s,%s,%s,%d,%d)",(id,firstname,lastname,allergy,allergycount,emernum))
 con.commit()
 
 cur.close()
