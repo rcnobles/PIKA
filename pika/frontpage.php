1c8f430c11d7c8f853f3462b66f6c96bd6c6a494
BrainiaK1911 <medadnobles@gmail.com>
2021-11-18 19:51:28 -0800

    Navigation
    Update main navigation
    Added text for more realistic user experience

--- pika/frontpage.php
+++ pika/frontpage.php
@@ -5,18 +5,18 @@
   </head>
   <body>
   <ul> <!-- Adds the navigation bar. Feel free to add more tabs as you see fit! -->
-      <li><a href="#home">Home</a></li>
+      <li><a href="frontpage.php">Home</a></li>
       <li><a href="about.html">About</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#research">Research</a></li>
+      <li><a href="services.html">Services</a></li>
+      <li><a href="research.html">Research</a></li>
       <li><a href="contact.html">Contact</a></li>
-      <li><a herf="index.php">Sign In</a></li>
+      <li><a herf="/">Sign In</a></li>
     </ul>
     <div class="center">
       <h1>Allergy List</h1>
       <form method="post">
         <div class="info">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor
+            View, Edit, and Share your list of Allergies. 
          </div>
          <button class="btn"> <a href="allergy.php">View Allergy List</a> </button>
       </form>
@@ -25,7 +25,7 @@
         <h1>View Data</h1>
         <form method="post">
           <div class="info">
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor
+            View and Share your Data!
            </div>
           <input type="submit" value="View Your Data">
         </form>
